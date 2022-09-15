814ace181091613a9968658e83eb17e3f2bae77f
carine ferreira da silva <carine.silvaf1@gmail.com>
2022-09-15 20:15:17 -0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 function addingEventListener() {
+    const x = document.getElementById("button")
+    x.addEventListener("click", clickEvent)
+
 }
+function clickEvent (){
+    console.log("this is a log")
+}
+
+addingEventListener()
